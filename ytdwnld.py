cd2dc377aeb7392c0de73a1d151d73aec37bb028
Mehul-grg <72182518+Mehul-grg@users.noreply.github.com>
2021-10-14 01:00:01 +0530

    Add files via upload

---
+++ ytdwnld.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+
+link = input("Enter the link:  ")
+yt = YouTube(link)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+
+for i in video:
+    print(i)
+
+print("Enter the desired option to download the format")
+dn_option = int(input("Enter the option: "))
+
+dn_video = videos[dn_option]
+dn_video.download()
+
+print("Download successfully")
